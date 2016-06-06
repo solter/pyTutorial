5505cb8e7ca4bbba85a6d7bb632abbd4b465cfcb
Peter Solfest <pmsolfest@gmail.com>
2016-06-05 22:00:16 -0500

    added fileIO demo

---
+++ src/loopsNds/fileIO.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+
+"""
+This module introduces some simple file io.
+It needs the demo.txt file.
+
+See the https://docs.python.org/3/tutorial/inputoutput.html documentation
+for more details (but beware as it mostly focuses on file io WITHOUT
+a context manager, which is not usually a good idea unles you have to).
+"""
+
+from pprint import pprint
+import numpy as np
+
+# This shows how to work with simple, unstructured files.
+# You will rarely encounter these, as usually a standard format
+# is used.
+
+### READING FILES
+
+# the open function with a 'r' opens up the file
+# for reading. By burying it inside a with, python
+# makes sure that the file is closed at the end
+fileTxt = ""
+with open('demo.txt','r') as f:
+    # read every line in the file, in a loop
+    for line in f:
+        # append the line into the fileTxt variable
+        fileTxt += "{}".format(line)        
+
+print(fileTxt)
+
+# parsing it into a dictionary
+standings = {}
+with open('demo.txt','r') as f:
+    # read the header
+    f.readline()
+    for line in f:
+        # split the line. defaults to splitting on whitespace
+        name, win, loss = line.split()
+        standings[name] = {'w':int(win), 'l':int(loss)}
+
+pprint(standings)
+
+### WRITING FILES
+# We can write the standings variable into a file using the following three methods (among others)
+
+# custom format
+# open a file for writing
+with open('demo2.txt','w') as f:
+    # write the header
+    f.write('name  win  loss\n')
+    for name, rec in standings.items():
+        f.write('{} {:d} {:d}\n'.format(name, rec['w'], rec['l']))
+
+# using the json format
+import json 
+with open('demo.json','w') as f:
+    # the indent option makes it more readable
+    json.dump(standings, f, indent=2)
+
+# read it back in with standings
+with open('demo.json','r') as f:
+    st2 = json.load(f)
+print('after json-ifying')
+print(st2)
+
+# using the pickle format
+# this is a binary format, so we have to open up a bytestream. hence the wb
+import pickle
+with open('demo.pkl','wb') as f:
+    pickle.dump(standings,f)
+
+with open('demo.pkl','rb') as f:
+    st3 = pickle.load(f)
+print('after pickling')
+print(st3)
+
+### NUMPY CONVENIENCE READER
+
+# Numpy also has a built in function to read in a structured text file,
+# See the http://docs.scipy.org/doc/numpy-1.10.0/reference/generated/numpy.genfromtxt.html
+# documentation.
+standings_np = np.genfromtxt('demo.txt', 
+                             dtype=[('name','S10'),('w','i8'),('l','i8')], 
+                             skip_header=1)
+print(standings_np)
+print('names')
+print(standings_np['name'])
+
+
