d162bd147a1e066aa8e618561531a771f3fa7977
Peter Solfest <pmsolfest@gmail.com>
2016-06-05 21:22:32 -0500

    added loops and numpy array demo

---
+++ src/loopsNds/loops.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python3
+
+"""
+This script is intended to demonstrate how python handles loops and
+complex if statements.
+The 2 (+1) loop types are:
+    for loops
+    while loops
+    list comprehensions
+
+When writing these lines in interactive mode,
+for indented blocks hit enter at the end of a newline and
+indent the code using spaces.
+A blank line terminates the indentation level and thus executes
+the block of code.
+
+e.g.
+>>> i = 1
+>>> while(i < 10):
+...   i += 1
+...
+>>>
+
+See the https://docs.python.org/3/tutorial/controlflow.html page
+"""
+
+### If blocks
+
+# define some boolean expressions
+a = True
+b = True
+c = False
+d = False
+
+# The most obvious if blocks use boolean expressions.
+if a: # if a is true
+    print('a is true')
+
+
+if a and b: # if both a and b are true
+    print('a and b are true')
+
+if not a and b: # if (not a) is true and b is true = if a is false and b is true
+    print('a false and b true')
+
+# as expressions get longer and more complicated, parenthesis for clarification
+# are HIGHLY recommended
+if (a and (b or c or d)): # if a is true and (at least one of b, c, d is true)
+    print('a and something else is true')
+
+if c == d: # if c equals d
+    print('c equals d')
+
+if a != d: # if a does not equal d
+    print('a does not equal d')
+
+# We can also do something if the expression is not met
+if a:
+    print('a is true')
+else:
+    print('a is false')
+
+# We can also check multiple states, and chain them indefinitely
+if a and b and c and d: # if they are all true
+    print('everything is true')
+elif c: # if the above condition is not met, and c is true
+    print('c is true')
+elif b: # if the above condition is not met, and b is true
+    print('b is true')
+else: # if none of the above conditions are met
+    print('the above conditions all failed')
+
+# For numbers (and anything properly defined, see documentation) we can
+# use the <, >, <=, and >= comparisons
+a = 1
+b = 2
+c = 3
+d = 4
+if a < b and d < c:
+    print('a < b and d < c')
+elif a <= b and d <= c:
+    print('a <= b and d <= c')
+elif a <= b < c: # we can also chain comparisons together
+    print('a <= b and b < c')
+else:
+    print('the above conditions fail')
+
+# See https://docs.python.org/3/reference/expressions.html#bComparisons for a complete explanation
+# of comparisons
+
+### While loops
+
+# A while loop repeats a set of instructions until the condition is false.
+# The condition can be anything that ends in a boolean, just like if blocks.
+# The instruction set is enclosed by an indentation block
+
+print('while loop')
+i = 1
+print(i)
+while (i < 10):
+    # comments are allowed in loops
+
+    # as are empty lines
+    i *= 2
+    print(i)
+
+print(i)
+
+### FOR LOOPS
+
+# For loops will loop through a tuple or list, or you can construct range objects for it to loop through
+
+# A range object is like a slice in a list. It starts at 0 (or the first argument if multiple are given),
+# and goes up to (not including) the last value in step sizes of 1 (or the third argument)
+
+print('range tests')
+print('range(5)')
+for i in range(5):
+    print(i)
+
+print('range(2,5)')
+for i in range(2,5):
+    print(i)
+
+print('range(1,7,2)')
+for i in range(1,7,2):
+    print(i)
+
+# You can turn a range into a list with the list function
+print('list from range')
+a = list(range(1,7,2))
+print(a)
+
+# we can loop through the list, and manipulate the index
+print('modifying loop index')
+print(a)
+for ele in a:
+    # This modifies ele, but NOT the element inside a
+    ele += 1
+    print(ele)
+print(a)
+
+# to modify the value inside a, we can loop through its index range
+print('modifying a')
+print(a)
+for i in range(len(a)):
+    a[i] += 1
+    print(a[i])
+print(a)
+
+# Simple list comprehension
+# Instead of using an explicit for loop, we can build a list through
+# list comprehensions. They act remarkable similar to for loops.
+# The choice of which to use should be decided by readability usually
+# There are some gotchas, especially for nested list comprehensions
+# See https://docs.python.org/3/tutorial/datastructures.html#list-comprehensions for
+# a more thorough description.
+print('form b as a**2 using list comprehension')
+b = [ele**2 for ele in a]
+print('a: {}'.format(a))
+print('b: {}'.format(b))
+
+# there are a variety of ways to loop through a dictionary, as below
+print('\n\ndictionary loops\n')
+d = {'a':1,'b':2,'c':3}
+print('the dictionary')
+print(d)
+
+print('loop through keys')
+for key in d.keys():
+    print(key)
+
+print('loop through values')
+for val in d.values():
+    print(val)
+
+print('loop through key value pairs')
+for key, val in d.items():
+    print('key: {}, val: {}'.format(key, val))
+
+print('modify the dictionary values')
+for key, val in d.items():
+    d[key] = val + 1
+print(d)
---
+++ src/loopsNds/numpy_arr.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+
+"""
+This script should show some of the power
+of numpy arrays, and demonstrate their usage.
+"""
+
+import numpy as np
+
+### The numpy array
+
+# Numpy arrays are defined with an initial size, which cannot
+# change without redefining the array. Furthermore, all elements
+# of an array are of the same type, and must be a number. By
+# default, this uses a float
+
+# We can start by building an array with each element equal to 1
+# it will have 2 dimensions, the first of which is 3 and the second
+# of which is 4. Typically, this is thought of as 3 rows with 4 elements
+# per row (4 columns), similiar to matrices.
+a = np.ones((3,4))
+print(a)
+
+# Let us define an array b, and fill it with a multiplication table
+b = np.zeros((3,4))
+print(b)
+# every array has a shape attribute, which stores its shape as tuple
+print(b.shape)
+for i in range(b.shape[0]):
+    for j in range(b.shape[1]):
+        # We can access the i row, j column entry using
+        # either b[i][j], or b[i,j]
+        b[i,j] = i*j
+print(b)
+
+# We can perform numerical operations on array, and they happen element-wise.
+# So squaring b is equivalent to squaring each element of b
+print('a')
+print(a)
+print('b')
+print(b)
+print('b**2')
+print(b**2)
+print('a + b')
+print(a+b)
+print('a*b')
+print(a*b)
+print('3*b - 2')
+print(3*b - 2)
+
+# Numpy has functions that will work element-wise on arrays
+print('sqrt(b)')
+print(np.sqrt(b))
+print('arctan(b) in degrees')
+print(np.rad2deg(np.arctan(b))) 
+
+# arithmetic does NOT work on arrays of different shapes if they both have
+# the same dimension
+
+# Turn a on its side
+a = a.transpose()
+print('a transpose')
+print(a)
+
+print('a + b')
+try:
+    print(a + b)
+except Exception as e:
+    print(e.args)
+
+# But consider the following 0-dimension array
+c = np.zeros((3,))
+for i in range(c.shape[0]):
+    c[i] = i+1
+d = np.zeros((4,))
+for i in range(d.shape[0]):
+    d[i] = i+5
+
+print('c')
+print(c)
+print('d')
+print(d)
+
+# Numpy will try to do a sensible 'broadcasting' to make arithemetic possible
+print('b + c')
+try:
+    print(b + c)
+except Exception as e:
+    print(e.args)
+print('b + d')
+print(b + d)
+
+# BEWARE THE NUMPY BROADCAST. it can be helpful, but also leads to wierd bugs that are hard to track down
+
+# We can also tranform numpy arrays to lists, and vice versa
+
+# array to list. This transform preserves the [i][j] indexing
+b_list = b.tolist()
+print('list b')
+print(b_list)
+
+# list to array
+c = np.array(b_list)
+print('c array = np.array(b_list)')
+print(c)
+
+# BEWARE THE LIST/ARRAY CONFUSION. 
+
+#Multiplying a list by a number duplicates the list
+# it does NOT multiply every element. It is easy to get this mixed up if you transform
+# between these often.
+print('b*3')
+print(b*3)
+print('np.array((list b)*3)')
+print(np.array(b_list*3))
+
+# You cannot add a non-list to a list, but you can for an array
+print('b + 1')
+print(b + 1)
+print('(list b) + 1')
+try:
+    print(b_list + 1)
+except Exception as e:
+    print(e.args)
